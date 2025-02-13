16fe9999b83e0303b38003cee669ee08da03f257
devNathanHub <mnangatnathan01@gmail.com>
2025-02-13 06:19:34 +0300

    lil change

--- app/api/posts/[id]/route.ts
+++ app/api/posts/[id]/route.ts
@@ -1,24 +1,35 @@
-import { NextResponse } from 'next/server';
-import prisma from '@/app/lib/prisma';
+import { NextRequest, NextResponse } from "next/server";
+import prisma from "@/app/lib/prisma";
 
 // GET Post by ID
-export async function GET(req: Request, { params }: { params: { id: string } }) {
+export async function GET(
+  req: NextRequest,
+  { params }: { params: { id: string } }
+) {
   try {
     const post = await prisma.post.findUnique({
       where: { id: params.id },
       include: { comments: true },
     });
 
-    if (!post) return NextResponse.json({ error: 'Post not found' }, { status: 404 });
+    if (!post) {
+      return NextResponse.json({ error: "Post not found" }, { status: 404 });
+    }
 
     return NextResponse.json(post);
   } catch (error) {
-    return NextResponse.json({ error: 'Error fetching post' }, { status: 400 });
+    return NextResponse.json(
+      { error: "Error fetching post" },
+      { status: 500 }
+    );
   }
 }
 
 // UPDATE Post
-export async function PUT(req: Request, { params }: { params: { id: string } }) {
+export async function PUT(
+  req: NextRequest,
+  { params }: { params: { id: string } }
+) {
   try {
     const { title, body } = await req.json();
     const updatedPost = await prisma.post.update({
@@ -28,19 +39,28 @@ export async function PUT(req: Request, { params }: { params: { id: string } }) 
 
     return NextResponse.json(updatedPost);
   } catch (error) {
-    return NextResponse.json({ error: 'Error updating post' }, { status: 400 });
+    return NextResponse.json(
+      { error: "Error updating post" },
+      { status: 500 }
+    );
   }
 }
 
 // DELETE Post
-export async function DELETE(req: Request, { params }: { params: { id: string } }) {
+export async function DELETE(
+  req: NextRequest,
+  { params }: { params: { id: string } }
+) {
   try {
     await prisma.post.delete({
       where: { id: params.id },
     });
 
-    return NextResponse.json({ message: 'Post deleted' });
+    return NextResponse.json({ message: "Post deleted" });
   } catch (error) {
-    return NextResponse.json({ error: 'Error deleting post' }, { status: 400 });
+    return NextResponse.json(
+      { error: "Error deleting post" },
+      { status: 500 }
+    );
   }
 }
